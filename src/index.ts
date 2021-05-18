06e099a19b2fcbfc59a2007a494171b032a28f95
Luiz Prianti <37250746+priantf@users.noreply.github.com>
2021-05-17 21:03:44 -0300

    Uploading the code created during class

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { UserEdit } from './views/UserEdit';
+import { User } from './models/User';
+
+const user = User.buildUser({ name: 'NAME', age: 20 });
+const userEdit = new UserEdit(
+    document.getElementById('root'),
+    user
+);
+
+userEdit.render();
+
+console.log(userEdit);
+
---
+++ src/models/ApiSync.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosPromise } from 'axios';
+
+interface HasId {
+    id?: number;
+}
+
+export class ApiSync<T extends HasId> {
+
+    constructor(public rooUrl: string) {}
+    fetch(id: number): AxiosPromise {
+        return axios.get(`${this.rooUrl}/${id}`);
+    }
+  
+    save(data: T): AxiosPromise {
+      const { id } = data;
+
+      if(id){
+        return axios.put(`${this.rooUrl}/${id}`, data);
+      }else{
+        return axios.post(this.rooUrl, data);
+      }
+    }
+}
